627016958f2bddd2dc6f1228cefab3ddd3b7d2cf
Kevin <kevinvosburgh4@gmail.com>
2021-12-12 11:23:48 -0500

    Remove overused function call with in place work using by reference only

--- 2021/Python/day12/part2Solution.py
+++ 2021/Python/day12/part2Solution.py
@@ -3,6 +3,7 @@ special_nodes = ["start", "end"]
 
 class Node:
     def __init__(self, key):
+        self.key = key
         self.is_lower = key.islower()
         self.is_start = key == "start"
         self.is_end = key == "end"
@@ -14,25 +15,23 @@ class Node:
         self.edges.append(node)
 
 
-def check_visit_count_okay(node, seen_small_cave_twice):
-    if node.is_lower:
-        if node.count == 1 and not seen_small_cave_twice and not node.is_start:
-            return True, True
-        if node.count > 0:
-            return False, seen_small_cave_twice
-    return True, seen_small_cave_twice
-
-
 def path_hunt(start, graph, seen_small_cave_twice=False):
     paths = 0
     for edge in start.edges:
-        can_continue, new_seen_small_cave_twice = check_visit_count_okay(edge, seen_small_cave_twice)
-        if can_continue:
-            if edge.is_end:
-                paths += 1
-            else:
+        if edge.is_end:
+            paths += 1
+        elif not edge.is_lower:
+            edge.count += 1
+            paths += path_hunt(edge, graph, seen_small_cave_twice)
+            edge.count -= 1
+        else:
+            if edge.count == 1 and not seen_small_cave_twice:
                 edge.count += 1
-                paths += path_hunt(edge, graph, new_seen_small_cave_twice)
+                paths += path_hunt(edge, graph, True)
+                edge.count -= 1
+            elif edge.count == 0:
+                edge.count += 1
+                paths += path_hunt(edge, graph, seen_small_cave_twice)
                 edge.count -= 1
     return paths
 
@@ -48,7 +47,8 @@ def main(lines):
             graph[second] = Node(second)
 
         graph[first].add_edge(graph[second])
-        graph[second].add_edge(graph[first])
+        if not graph[first].is_start:
+            graph[second].add_edge(graph[first])
 
     start = graph["start"]
     start.count = 1
