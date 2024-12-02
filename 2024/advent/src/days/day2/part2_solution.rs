17b5215b16ebcc4e72224356113dabf1880eef8f
Kevin <kpvosburgh@gmail.com>
2024-12-02 07:23:08 -0500

    Solve day2 part2

--- 2024/advent/src/days/day2/part2_solution.rs
+++ 2024/advent/src/days/day2/part2_solution.rs
@@ -1,24 +1,58 @@
-use std::collections::HashMap;
+fn are_safe_values(value_1: i32, value_2: i32, direction: i32) -> bool {
+    let difference = value_2 - value_1;
+    // handle case where values can only step by 1 - 3
+    if difference.abs() < 1 || difference.abs() > 3 {
+        return false;
+    }
+
+    // handle only increasing/decreasing check
+    if difference > 0 && direction == -1 {
+        return false;
+    }
+    if difference < 0 && direction == 1 {
+        return false;
+    }
+    return true;
+}
+
+fn line_is_safe(items: &Vec<i32>) -> bool {
+    let direction = if items[1] - items[0] > 0 { 1 } else { -1 };
+    for value_index in 1..items.len() {
+        if !are_safe_values(items[value_index - 1], items[value_index], direction) {
+            return false;
+        }
+    }
+    return true;
+}
 
 #[allow(dead_code)]
 pub fn solve(inputs: &Vec<String>) -> String {
-    let mut left: Vec<i32> = Vec::new();
-    let mut right: HashMap<i32, i32> = HashMap::new();
+    let mut safe_count = 0;
 
     for line in inputs {
-        let split: Vec<&str> = line.split(" ").collect();
-        left.push(split.first().unwrap().parse().unwrap());
-        let right_value: i32 = split.last().unwrap().parse().unwrap();
-        right
-            .entry(right_value)
-            .and_modify(|val| *val += 1)
-            .or_insert(1);
+        let split: Vec<i32> = line
+            .split(" ")
+            .map(|item| item.parse::<i32>().unwrap())
+            .collect();
+
+        let is_safe = line_is_safe(&split);
+        if is_safe {
+            safe_count += 1;
+            continue;
+        }
+
+        //try removing one index at a time
+        for delete_index in 0..split.len() {
+            let mut cloned = split.clone();
+            cloned.remove(delete_index);
+
+            let is_safe = line_is_safe(&cloned);
+            if is_safe {
+                safe_count += 1;
+                break;
+            }
+        }
     }
 
-    let mut cummulative_score = 0;
-    for number in left {
-        let right_count = right.entry(number).or_default();
-        cummulative_score += number * *right_count;
-    }
-    return format!("{}", cummulative_score);
+    return format!("{}", safe_count);
 }
--- 2024/advent/src/days/day2/test_part2.rs
+++ 2024/advent/src/days/day2/test_part2.rs
@@ -3,15 +3,25 @@ use crate::days::day2::part2_solution;
 
 #[test]
 fn example() {
-    let inputs = "3   4
-4   3
-2   5
-1   3
-3   9
-3   3"
+    let inputs = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"
         .split("\n")
         .map(|temp: &str| -> String { temp.to_string() })
         .collect();
     let result = part2_solution::solve(&inputs);
-    assert_eq!(result, "31")
+    assert_eq!(result, "4")
+}
+
+#[test]
+fn special_case_1() {
+    let inputs = "8 2 3 5 6"
+        .split("\n")
+        .map(|temp: &str| -> String { temp.to_string() })
+        .collect();
+    let result = part2_solution::solve(&inputs);
+    assert_eq!(result, "1")
 }
