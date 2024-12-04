82f5718b4c8c2df516092046d58f04be3efe8d0b
Kevin <kpvosburgh@gmail.com>
2024-12-03 20:40:24 -0500

    Finally finished day 3....

--- 2024/advent/src/days/day3/part2_solution.rs
+++ 2024/advent/src/days/day3/part2_solution.rs
@@ -1,56 +1,28 @@
 use regex::Regex;
 
-fn get_sum_for_slice(search: &str) -> i32 {
-    let re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
-    let mut sum = 0;
-    for m in re.captures_iter(&search) {
-        let first_num: i32 = m.get(1).unwrap().as_str().parse().unwrap();
-        let second_num: i32 = m.get(2).unwrap().as_str().parse().unwrap();
-
-        sum += first_num * second_num
-    }
-    sum
-}
-
 #[allow(dead_code)]
 pub fn solve(inputs: &Vec<String>) -> String {
     let mut cumulative_sum = 0;
+    let mut enabled = true;
 
     for line in inputs {
-        println!("----------------line diff --------------");
-        let mut index = 0;
-        let mut enabled = true;
-        while index < line.len() {
-            if enabled {
-                let next_bad_index = line[index..].find("don't()");
-                let next_index = match next_bad_index {
-                    None => line.len(),
-                    Some(i) => index + i + 7,
-                };
-                println!(
-                    "do sum for {} to {}, {} {}",
-                    index,
-                    next_index,
-                    line.len(),
-                    &line[index..next_index]
-                );
-                cumulative_sum += get_sum_for_slice(&line[index..next_index]);
-                index = next_index;
-                enabled = false;
-            } else {
-                let next_good_index = line[index..].find("do()");
-                let next_index = match next_good_index {
-                    None => line.len(),
-                    Some(i) => index + i + 4,
-                };
-                index = next_index;
-                enabled = true;
+        let re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)|do\(\)|don't\(\)").unwrap();
+
+        for m in re.captures_iter(&line) {
+            let statement = m.get(0).unwrap().as_str();
+            if statement == "do()" {
+                enabled = true
+            } else if statement == "don't()" {
+                enabled = false
+            } else if enabled {
+                println!("{}", statement);
+                let first_num: i32 = m.get(1).unwrap().as_str().parse().unwrap();
+                let second_num: i32 = m.get(2).unwrap().as_str().parse().unwrap();
+
+                cumulative_sum += first_num * second_num
             }
         }
     }
 
     cumulative_sum.to_string()
 }
-
-// too high 92974346
-// original 185797128
