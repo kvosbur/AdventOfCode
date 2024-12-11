0225317c923713467de81bf066a26ed57221ebce
Kevin <kpvosburgh@gmail.com>
2024-12-11 07:21:29 -0500

    Solved day 10 part 2

--- 2024/advent/src/days/day10/part2_solution.rs
+++ 2024/advent/src/days/day10/part2_solution.rs
@@ -1,92 +1,49 @@
-#[derive(Debug)]
-struct DiskNode {
-    used_pages: Vec<Option<u64>>,
-    original_used_length: u32,
-    free_pages: u32,
-}
-
-fn read_nodes(bytes: Vec<u8>) -> Vec<DiskNode> {
-    let mut disk_nodes: Vec<DiskNode> = Vec::new();
-    for start_index in (0..bytes.len()).step_by(2) {
-        let file_id = start_index / 2;
-        let used_blocks = bytes[start_index] - b'0';
-        let free_blocks: u8;
-        if start_index + 1 == bytes.len() {
-            free_blocks = 0;
-        } else {
-            free_blocks = bytes[start_index + 1] - b'0';
+fn score_trailhead(start_x: usize, start_y: usize, characters: &Vec<&[u8]>) -> usize {
+    let mut locations = vec![(start_x, start_y, b'0')];
+    let mut trailhead_score = 0;
+    while locations.len() > 0 {
+        let (x_loc, y_loc, value) = locations.pop().unwrap();
+        if value == b'9' {
+            trailhead_score += 1;
+            continue;
         }
-        let used_blocks_vec: Vec<Option<u64>> = (0..used_blocks)
-            .map(|_r| Some(file_id.try_into().unwrap()))
-            .collect();
-        disk_nodes.push(DiskNode {
-            original_used_length: used_blocks_vec.len().try_into().unwrap(),
-            used_pages: used_blocks_vec,
-            free_pages: free_blocks.try_into().unwrap(),
-        });
-    }
-    return disk_nodes;
-}
+        let target_value = value + 1;
 
-fn get_checksum(disk_nodes: &Vec<DiskNode>) -> u64 {
-    let mut checksum: u64 = 0;
-    let mut overall_index: u64 = 0;
-    for node in disk_nodes {
-        for block in &node.used_pages {
-            if block.is_some() {
-                checksum += block.unwrap() * (overall_index);
-            }
-
-            overall_index += 1;
+        // check left
+        if x_loc > 0 && characters[x_loc - 1][y_loc] == target_value {
+            locations.push((x_loc - 1, y_loc, target_value));
         }
-        let index_increase_amount: u64 = node.free_pages.try_into().unwrap();
-        overall_index += index_increase_amount;
+        // check down
+        if y_loc > 0 && characters[x_loc][y_loc - 1] == target_value {
+            locations.push((x_loc, y_loc - 1, target_value));
+        }
+
+        // check right
+        if x_loc < characters.len() - 1 && characters[x_loc + 1][y_loc] == target_value {
+            locations.push((x_loc + 1, y_loc, target_value));
+        }
+
+        // check up
+        if y_loc < characters[0].len() - 1 && characters[x_loc][y_loc + 1] == target_value {
+            locations.push((x_loc, y_loc + 1, target_value));
+        }
     }
-    return checksum;
+
+    trailhead_score
 }
 
 #[allow(dead_code)]
 pub fn solve(inputs: &Vec<String>) -> String {
-    let bytes = inputs[0].clone().into_bytes();
+    let input_chars: Vec<&[u8]> = inputs.iter().map(|s| s.as_bytes()).collect();
 
-    let mut disk_nodes: Vec<DiskNode> = read_nodes(bytes);
-
-    let mut right_node_index = disk_nodes.len() - 1;
-
-    while right_node_index > 0 {
-        let right_node_read = &disk_nodes[right_node_index];
-        if right_node_read.used_pages.len() == 0 {
-            right_node_index -= 1;
-            continue;
-        }
-        let mut left_node_index = 0;
-        while left_node_index < right_node_index {
-            let left_node_read = &disk_nodes[left_node_index];
-            if left_node_read.free_pages < right_node_read.original_used_length {
-                left_node_index += 1;
-                continue;
+    let mut trailhead_score_sum = 0;
+    for x in 0..input_chars.len() {
+        for y in 0..input_chars[0].len() {
+            if input_chars[x][y] == b'0' {
+                trailhead_score_sum += score_trailhead(x, y, &input_chars);
             }
-
-            let mut right_moving = right_node_read.used_pages.clone();
-            let right_moving_count = right_node_read.original_used_length.clone();
-
-            let left_node_write = &mut disk_nodes[left_node_index];
-
-            for remove_index in 0..right_moving_count {
-                let i: usize = remove_index.try_into().unwrap();
-                left_node_write.used_pages.push(right_moving[i]);
-                right_moving[i] = None;
-                left_node_write.free_pages -= 1;
-            }
-
-            let right_node_write = &mut disk_nodes[right_node_index];
-            right_node_write.used_pages = right_moving;
-            break;
         }
-        right_node_index -= 1;
     }
 
-    get_checksum(&disk_nodes).to_string()
+    trailhead_score_sum.to_string()
 }
-
-// currently getting too high of a value
--- 2024/advent/src/days/day10/test_part2.rs
+++ 2024/advent/src/days/day10/test_part2.rs
@@ -3,20 +3,48 @@ use crate::days::day10::part2_solution;
 
 #[test]
 fn example() {
-    let inputs = "2333133121414131402"
+    let inputs = "..90..9
+...1.98
+...2..7
+6543456
+765.987
+876....
+987...."
         .split("\n")
         .map(|temp: &str| -> String { temp.to_string() })
         .collect();
     let result = part2_solution::solve(&inputs);
-    assert_eq!(result, "2858")
+    assert_eq!(result, "13")
 }
 
 #[test]
 fn example2() {
-    let inputs = "12345"
+    let inputs = "012345
+123456
+234567
+345678
+4.6789
+56789."
         .split("\n")
         .map(|temp: &str| -> String { temp.to_string() })
         .collect();
     let result = part2_solution::solve(&inputs);
-    assert_eq!(result, "132")
+    assert_eq!(result, "227")
+}
+
+#[test]
+fn example3() {
+    let inputs = "89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732"
+        .split("\n")
+        .map(|temp: &str| -> String { temp.to_string() })
+        .collect();
+    let result = part2_solution::solve(&inputs);
+    assert_eq!(result, "81")
 }
