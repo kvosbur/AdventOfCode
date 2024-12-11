a5c25ff411d5093d3269ca8c312942fd0b00a443
Kevin <kpvosburgh@gmail.com>
2024-12-11 11:54:41 -0500

    Brute force for day11 part2 still chugging

--- 2024/advent/src/days/day11/part2_solution.rs
+++ 2024/advent/src/days/day11/part2_solution.rs
@@ -1,49 +1,87 @@
-fn score_trailhead(start_x: usize, start_y: usize, characters: &Vec<&[u8]>) -> usize {
-    let mut locations = vec![(start_x, start_y, b'0')];
-    let mut trailhead_score = 0;
-    while locations.len() > 0 {
-        let (x_loc, y_loc, value) = locations.pop().unwrap();
-        if value == b'9' {
-            trailhead_score += 1;
-            continue;
-        }
-        let target_value = value + 1;
+use std::collections::HashMap;
 
-        // check left
-        if x_loc > 0 && characters[x_loc - 1][y_loc] == target_value {
-            locations.push((x_loc - 1, y_loc, target_value));
-        }
-        // check down
-        if y_loc > 0 && characters[x_loc][y_loc - 1] == target_value {
-            locations.push((x_loc, y_loc - 1, target_value));
-        }
+struct StoneCounting {
+    step_count: u128,
+    stone_map: HashMap<u128, Vec<u128>>,
+    stone_count: u128,
+}
 
-        // check right
-        if x_loc < characters.len() - 1 && characters[x_loc + 1][y_loc] == target_value {
-            locations.push((x_loc + 1, y_loc, target_value));
-        }
+fn get_stones(value: u128, step_count: u128) -> Vec<u128> {
+    let mut stones = vec![value];
+    let mut mutable_step_count = step_count;
 
-        // check up
-        if y_loc < characters[0].len() - 1 && characters[x_loc][y_loc + 1] == target_value {
-            locations.push((x_loc, y_loc + 1, target_value));
+    while mutable_step_count > 0 {
+        let mut new_stones: Vec<u128> = Vec::new();
+
+        for stone in stones {
+            let stone_string = stone.to_string();
+            if stone == 0 {
+                new_stones.push(1);
+            } else if stone_string.len() % 2 == 0 {
+                let first_val: u128 = stone_string[..stone_string.len() / 2].parse().unwrap();
+                let second_val: u128 = stone_string[stone_string.len() / 2..].parse().unwrap();
+                new_stones.push(first_val);
+                new_stones.push(second_val);
+            } else {
+                new_stones.push(stone * 2024);
+            }
         }
+        stones = new_stones;
+        mutable_step_count -= 1;
+    }
+    stones
+}
+
+fn get_stone_count(
+    value: u128,
+    step_count: u128,
+    step_count_step: u128,
+    stone_counting: &mut StoneCounting,
+) -> (u128, &mut StoneCounting) {
+    let mut stone_count = 0;
+    let mut temp = stone_counting;
+
+    if !temp.stone_map.contains_key(&value) {
+        temp.stone_map
+            .insert(value, get_stones(value, step_count_step));
+    }
+    let stones: Vec<u128> = temp.stone_map.get(&value).unwrap().clone();
+
+    if step_count > 59 {
+        println!("{} val:{}  stones: {}", step_count, value, stones.len());
     }
 
-    trailhead_score
+    if step_count == step_count_step {
+        return (stones.len().try_into().unwrap(), temp);
+    }
+
+    for stone in stones {
+        let count: u128;
+        (count, temp) = get_stone_count(stone, step_count - step_count_step, step_count_step, temp);
+        stone_count += count;
+    }
+
+    (stone_count, temp)
 }
 
 #[allow(dead_code)]
 pub fn solve(inputs: &Vec<String>) -> String {
-    let input_chars: Vec<&[u8]> = inputs.iter().map(|s| s.as_bytes()).collect();
+    let start_values: Vec<u128> = inputs[0]
+        .split(" ")
+        .map(|val| val.parse().unwrap())
+        .collect();
 
-    let mut trailhead_score_sum = 0;
-    for x in 0..input_chars.len() {
-        for y in 0..input_chars[0].len() {
-            if input_chars[x][y] == b'0' {
-                trailhead_score_sum += score_trailhead(x, y, &input_chars);
-            }
-        }
+    let mut stone_count = 0;
+    let mut stone_counting = StoneCounting {
+        step_count: 0,
+        stone_map: HashMap::new(),
+        stone_count: 0,
+    };
+    for val in start_values {
+        let temp_stone_count: u128;
+        (temp_stone_count, _) = get_stone_count(val, 75, 15, &mut stone_counting);
+        // let stones = get_stones(val, 75);
+        stone_count += temp_stone_count;
     }
-
-    trailhead_score_sum.to_string()
+    stone_count.to_string()
 }
